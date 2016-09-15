2656c825cd8b79a88a753a0838f3cb898ff263fe
David Ebinger <David Ebinger>
2016-09-15 13:42:17 -1000

    Object 3-CompletePerson

--- objects.js
+++ objects.js
@@ -12,6 +12,11 @@ plainBox.size = 8;
 plainBox.contents = [];
 
 console.log(plainBox);
+console.log(plainBox.color);
+console.log(plainBox.size);
+console.log(plainBox.contents);
+
+
 /*
     # An object with properties declared line by line
 
@@ -34,6 +39,11 @@ var stockCar = {
     passengers: []
 };
 console.log(stockCar);
+console.log(stockCar.model);
+console.log(stockCar.year);
+console.log(stockCar.automaticTransmission);
+console.log(stockCar.driver);
+console.log(stockCar.passengers);
 /*
     Add new property inside a function
 
@@ -54,6 +64,12 @@ function buildPerson(person, nameString, age) {
     return person;
 }
 console.log(buildPerson(plainPerson,"Luke",30));
+
+var completePerson = buildPerson(plainPerson, "Luke", 30);
+console.log(completePerson);
+console.log(completePerson.name);
+console.log(completePerson.age);
+
 /*
     # Display values of objects that are inside an array
         Declare a new variable named `arrayOfObjects` and set it to be [this array of objects](https://gist.github.com/sgnl/958adf99007329d2e4ff).
@@ -76,8 +92,62 @@ console.log(buildPerson(plainPerson,"Luke",30));
             =====
             ...
  */
-
-
+var arrayOfObjects = [
+  {
+    id: 0,
+    date: "Monday Jan 25 2015 2:01 PM",
+    total: "279.38"
+  },
+  {
+    id: 1,
+    date: "Monday Jan 27 2015 11:31 AM",
+    total: "79.80"
+  },
+  {
+    id: 2,
+    date: "Monday Feb 1 2015 7:56 AM",
+    total: "15.62"
+  },
+  {
+    id: 3,
+    date: "Monday Feb 1 2015 9:43 AM",
+    total: "19.83"
+  },
+  {
+    id: 4,
+    date: "Monday Feb 1 2015 11:08 PM",
+    total: "56.69"
+  },
+  {
+    id: 5,
+    date: "Monday Feb 13 2015 10:22 AM",
+    total: "137.92"
+  },
+  {
+    id: 6,
+    date: "Monday Feb 14 2015 6:54 PM",
+    total: "938.65"
+  },
+  {
+    id: 7,
+    date: "Monday Feb 14 2015 7:17 PM",
+    total: "43.77"
+  },
+  {
+    id: 8,
+    date: "Monday Feb 14 2015 7:18 PM",
+    total: "28.54"
+  },
+  {
+    id: 9,
+    date: "Monday Feb 14 2015 7:18 PM",
+    total: "194.33"
+  }
+];
+function printProcessedOrders(orders) {
+    return arrayOfObjects;
+}
+console.log(arrayOfObjects);
 /*
     # Addition with an object
         Declare a new variable named sumObj and set it to be a new object with the properties `a`, `b`, and `result`. Properties 'a' and 'b' will be set to a number and result will be 'undefined.'
